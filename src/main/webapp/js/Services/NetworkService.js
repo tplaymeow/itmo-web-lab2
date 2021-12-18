9aff1a6f5428b128af5de5b1517098eab81e41f6
Timur Guliamov <wersa2002@gmail.com>
2021-12-18 05:34:40 +0300

    replaced GET with POST

--- src/main/webapp/js/Services/NetworkService.js
+++ src/main/webapp/js/Services/NetworkService.js
@@ -1,14 +1,7 @@
 class NetworkService {
     fetchResult(coords, onSuccess, onFailure) {
-        $.get({
-            url: "https://se.ifmo.ru/~s311703/web_lab1/php/main.php",
-            contentType: "application/json;charset=utf-8",
-            data: coords,
-            success: (data) => {
-                console.log(data);
-                onSuccess(JSON.parse(JSON.stringify(data)));
-            },
-            error: onFailure
-        })
+        $.post("https://se.ifmo.ru/~s311703/web_lab1/php/main.php", coords)
+            .done(function(data){ onSuccess(JSON.parse(JSON.stringify(data))); })
+            .fail(function(xhr, status, error) { onFailure() });
     }
 }
